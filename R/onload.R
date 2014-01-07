ead614fdec825e468f26a32acaddbb23d0673aad
David Ardia <david.ardia@fsa.ulaval.ca>
2014-01-07 00:00:00 +0000

    version 2.0.1

--- R/onload.R
+++
@@ -1,7 +0,0 @@
-'.onLoad' <- function(lib, pkg="bayesGARCH")
-{
-  cat("\nLoading 'bayesGARCH' package...\n")
-  cat("Version 1.00-10\n")
-  cat("Bayesian Estimation of the GARCH(1,1) Model with Student-t Innovations\n")
-  cat("Author and maintainer: David Ardia <david.ardia@unifr.ch>\n")
-}
